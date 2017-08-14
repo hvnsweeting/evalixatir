12b18d7db02e881632549ca573814fbc5ca0dabd
Viet Hung Nguyen <hvn@familug.org>
2017-08-14 15:32:52 +0700

    Allow older version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Evalixatir.Mixfile do
     [
       app: :evalixatir,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.4",
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
